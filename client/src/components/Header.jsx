bbdc15505a036616a0b11df61487ba20be73be9d
Daniel <zavaladaniel151@gmail.com>
2024-03-06 23:29:25 -0500

    Updated Header Component
    let logo be a pathway to '/'

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,9 +10,11 @@ const Header = () => {
     return (
         <div className='d-flex justify-content-center flex-wrap align-items-center align-content-center flex-row m-3 pb-5 pt-2'  > 
         
-            {/* This is the Company Name */}
+            {/* This is the Company Logo */}
             <div className=' d-flex flex-fill justify-content-center'>
+                <a href='/'>
                 <img src={Logo} alt='Company Logo'></img>
+                </a>
                 {/* <h1 > {companyName}  </h1> */}
             </div>
 
